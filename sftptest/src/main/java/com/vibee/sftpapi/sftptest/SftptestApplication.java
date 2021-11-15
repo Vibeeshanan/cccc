db4c9de45fb84fd08b4e06b1147a61f34962f963
Vibeeshanan <94167852+Vibeeshanan@users.noreply.github.com>
2021-11-15 23:24:32 +0530

    Add files via upload

---
+++ sftptest/src/main/java/com/vibee/sftpapi/sftptest/SftptestApplication.java
@@ -0,0 +1,13 @@
+package com.vibee.sftpapi.sftptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftptestApplication.class, args);
+	}
+
+}
---
+++ sftptest/src/test/java/com/vibee/sftpapi/sftptest/SftptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibee.sftpapi.sftptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
